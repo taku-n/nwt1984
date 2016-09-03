0c88c31d6817a1bc51725d3697bf3d80401e4b07
root <shino@hoge.com>
2016-09-04 00:39:07 +0900

    there is a bug of invalid UTF-8, String#scrub

--- create.sh
+++ create.sh
@@ -1,48 +1,61 @@
 #!/usr/sbin/sh
 
-echo "processing html"
+# get the main text from html
+html2txt() {
+	echo "processing html"
 
-for htmlfile in $(find nwt1984html -type f)  # ディレクトリを除いたリスト
-do
-	output0=$(echo $htmlfile | cut -d "/" -f 2-3 | cut -d "." -f 1)
-	output="nwt1984txt/${output0}.txt"
+	for htmlfile in $(find nwt1984html -type f)  # ディレクトリを除いたリスト
+	do
+		output0=$(echo $htmlfile | cut -d "/" -f 2-3 | cut -d "." -f 1)
+		output="nwt1984txt/${output0}.txt"
 
-	echo "htmlfile"
-	echo $htmlfile
-	echo "output"
-	echo $output
+		echo "htmlfile"
+		echo $htmlfile
+		echo "output"
+		echo $output
 
-	./html2txt.rb $htmlfile $output
-done
+		./html2txt.rb $htmlfile $output
+	done
+}
 
-echo "mecab is running"
+# run MeCab for the main text
+run_mecab() {
+	echo "mecab is running"
 
-mkdir nwt1984out  # MeCab の出力ファイルを保持
+	mkdir nwt1984out  # MeCab の出力ファイルを保持
 
-for txtfile in $(find nwt1984txt -type f)  # ディレクトリを除いたリスト
-do
-	echo "txtfile"
-	echo $txtfile
+	for txtfile in $(find nwt1984txt -type f)  # ディレクトリを除いたリスト
+	do
+		echo "txtfile"
+		echo $txtfile
 
-	outfile0=$(echo $txtfile | cut -d "/" -f 2-3 | cut -d "." -f 1)
-	outfile="nwt1984out/${outfile0}.out"
+		outfile0=$(echo $txtfile | cut -d "/" -f 2-3 | cut -d "." -f 1)
+		outfile="nwt1984out/${outfile0}.out"
 
-	echo "outfile"
-	echo $outfile
+		echo "outfile"
+		echo $outfile
 
-	# 書名のディレクトリを作成する
-	dir_name=$(echo $txtfile | cut -d "/" -f 2)
-	mkdir "nwt1984out/${dir_name}"
+		# 書名のディレクトリを作成する
+		dir_name=$(echo $txtfile | cut -d "/" -f 2)
+		mkdir "nwt1984out/${dir_name}"
 
-	mecab $txtfile -o $outfile
-done
+		mecab $txtfile -o $outfile
+	done
+}
 
-echo "removing details"
+# removing details from the main text
+remove_details() {
+	echo "removing details"
 
-for mecab_out in $(find nwt1984out -type f)  # ディレクトリを除いたリスト
-do
-	final_output0=$(echo $mecab_out | cut -d "/" -f 2-3 | cut -d "." -f 1)
-	final_output="nwt1984/${final_output0}"
+	for mecab_out in $(find nwt1984out -type f)  # ディレクトリを除いたリスト
+	do
+		final_output0=$(echo $mecab_out | cut -d "/" -f 2-3 | cut -d "." -f 1)
+		final_output="nwt1984/${final_output0}"
 
-	./rm-details.rb $mecab_out $final_output
-done
+		./rm-details.rb $mecab_out $final_output
+	done
+}
+
+#html2txt
+#run_mecab
+remove_details
--- rm-details.rb
+++ rm-details.rb
@@ -21,6 +21,7 @@ print("textfile: ", textfile, "\n")
 
 File.open(textfile, "w") do |f|
 	input.each_line do |line|
+		line.scrub!('?')
 		tmp = line.sub(/\t.*/, "")
 		f.write(tmp)
 	end
